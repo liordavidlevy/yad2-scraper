698b8e8d25ffa3e0f655b57c56a07100ba6dfd13
liordavidlevy <liordavid20@gmail.com>
2025-03-18 22:38:44 +0200

    do stuff

--- scraper.js
+++ scraper.js
@@ -85,7 +85,6 @@ const scrape = async (topic, url) => {
     const chatId = process.env.CHAT_ID || config.chatId;
     const telenode = new Telenode({apiToken})
     try {
-        await telenode.sendTextMessage(`Starting scanning ${topic} on link:\n${url}`, chatId)
         const scrapeImgResults = await scrapeItemsAndExtractImgUrls(url);
         const newItems = await checkIfHasNewItem(scrapeImgResults, topic);
         if (newItems.length > 0) {
@@ -93,14 +92,12 @@ const scrape = async (topic, url) => {
             const msg = `${newItems.length} new items:\n${newItemsJoined}`
             await telenode.sendTextMessage(msg, chatId);
         } else {
-            await telenode.sendTextMessage("No new items were added", chatId);
         }
     } catch (e) {
         let errMsg = e?.message || "";
         if (errMsg) {
             errMsg = `Error: ${errMsg}`
         }
-        await telenode.sendTextMessage(`Scan workflow failed... 😥\n${errMsg}`, chatId)
         throw new Error(e)
     }
 }
